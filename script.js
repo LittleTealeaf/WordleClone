818c56835e45e89ca7c8e1a8bc9c383f5e33f071
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-06-08 11:41:01 -0400

    Stuff changes!

--- script.js
+++ script.js
@@ -5,54 +5,106 @@ const chars = "abcdefghijklmnopqrstuvwxyz".split("").map(str => str.charAt(0));
 const guesses = [
 ];
 
-var currentRow;
+var currentRow = [];
 var currentGuess = "";
 
 var word = "words";
 
+// function render() {
+//     guessesElement.innerHTML = "";
+//     guesses.forEach(item => {
+//         const row = document.createElement("tr");
+//         row.className = "row";
+
+//         item.split("").forEach((char, index) => {
+//             const c = document.createElement("td");
+
+
+//             if (word.charAt(index) == item.charAt(index)) {
+//                 c.className = "character correct"
+//             } else {
+//                 for (var i = 0; i <= word.length; i++) {
+//                     if (i == word.length) {
+//                         c.className = "character wrong";
+//                     } else if (word.charAt(i) == char.charAt(0)) {
+//                         c.className = "character partial";
+
+//                         break;
+//                     }
+//                 }
+//             }
+
+//             c.innerHTML = char;
+
+//             row.appendChild(c);
+
+
+//         });
+//         guessesElement.appendChild(row);
+//     });
+
+//     renderCurrent();
+// }
 function render() {
     guessesElement.innerHTML = "";
     guesses.forEach(item => {
         const row = document.createElement("tr");
         row.className = "row";
 
-        var w
 
-        item.split("").forEach((char, index) => {
-            const c = document.createElement("td");
+        var w = word.split("");
 
+        const values = item.split("").map(value => {
+            const td = document.createElement("td");
+            td.innerHTML = value;
+            td.classList.add("character");
+            row.appendChild(td);
+            return td;
+        });
 
-            if (word.charAt(index) == item.charAt(index)) {
-                c.className = "character correct"
-            } else {
-                for (var i = 0; i <= word.length; i++) {
-                    if (i == word.length) {
-                        c.className = "character wrong";
-                    } else if (word.charAt(i) == char.charAt(0)) {
-                        c.className = "character partial";
+        values.forEach((item, index) => {
+            if (word.charAt(index) == item.innerHTML.charAt(0)) {
+                item.classList.add("correct");
+                removeItemOnce(w,item.innerHTML);
+            }
+        });
 
+        values.forEach((item,index) => {
+
+            if(!item.classList.contains("correct")) {
+                for(var i in w) {
+                    if(w[i] == item.innerHTML) {
+                        item.classList.add("partial");
+                        removeItemOnce(w,item.innerHTML);
                         break;
                     }
                 }
             }
-
-            c.innerHTML = char;
-
-            row.appendChild(c);
-
-
         });
+
+
+        console.log(values);
         guessesElement.appendChild(row);
-    });
+    })
+
     currentRow = document.createElement("tr");
     currentRow.className = "row";
     guessesElement.appendChild(currentRow);
     renderCurrent();
 }
 
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+
 function renderCurrent() {
     currentRow.innerHTML = "";
-    currentGuess.split("").forEach((char,index) => {
+    currentGuess.split("").forEach((char, index) => {
         const c = document.createElement("td");
         c.innerHTML = char;
         c.className = "character default"
@@ -61,15 +113,15 @@ function renderCurrent() {
 }
 
 document.onkeydown = async (ev) => {
-    if(isAlpha(ev.key.toLowerCase())) {
-        if(currentGuess.length < 5) {
+    if (isAlpha(ev.key.toLowerCase())) {
+        if (currentGuess.length < 5) {
             currentGuess = currentGuess + ev.key;
         }
-    } else if(ev.key == "Backspace") {
-        if(currentGuess.length > 0) {
-            currentGuess = currentGuess.slice(0,-1);
+    } else if (ev.key == "Backspace") {
+        if (currentGuess.length > 0) {
+            currentGuess = currentGuess.slice(0, -1);
         }
-    } else if(ev.key == "Enter" && currentGuess.length == 5) {
+    } else if (ev.key == "Enter" && currentGuess.length == 5) {
         //
         // const valid = fetchWords().then(arr => arr.includes(currentGuess));
 
@@ -78,8 +130,8 @@ document.onkeydown = async (ev) => {
         // }
 
         const words = await fetchWords();
-        for(var i in words) {
-            if(currentGuess == words[i]) {
+        for (var i in words) {
+            if (currentGuess == words[i]) {
                 guesses.push(currentGuess);
                 currentGuess = "";
                 render();
@@ -89,9 +141,9 @@ document.onkeydown = async (ev) => {
     renderCurrent();
 }
 
-function contains(array,item){
-    for(var i in array) {
-        if(array[i] == item) {
+function contains(array, item) {
+    for (var i in array) {
+        if (array[i] == item) {
             return true;
         }
     }
@@ -99,9 +151,9 @@ function contains(array,item){
 }
 
 function isAlpha(val) {
-    for(var i in chars) {
+    for (var i in chars) {
 
-        if(val == chars[i]) {
+        if (val == chars[i]) {
             return true;
         }
     }
