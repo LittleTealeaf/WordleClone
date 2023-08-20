e1f7824f98640ca46986421dd581361fe957205c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-08-20 18:36:17 -0400

    formatting

--- populate_words.py
+++ populate_words.py
@@ -1,5 +1,6 @@
 import json
 
 with open('./words.txt') as f_in:
-    with open('./words.json','w') as f_out:
-        f_out.write(json.dumps([word.replace('\n','') for word in f_in.readlines()]))
+    with open('./words.json', 'w') as f_out:
+        f_out.write(json.dumps([word.replace('\n', '')
+                    for word in f_in.readlines()]))
